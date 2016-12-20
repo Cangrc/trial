902a1a684d61e25398ec12c0f3a0d04118f54c70
Can Giracoglu <cangiracoglu@gmail.com>
2016-12-21 00:47:38 +0300

    Create New Visual Studio Project

---
+++ HateorLove/HateorLove/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace HateorLove.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ HateorLove/HateorLove/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HateorLove.Global" Language="C#" %>
